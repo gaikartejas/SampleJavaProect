33e528a92cd79378077670d2a9b7470256bde2d6
TEJAS <gaikar.tejas22@gmgail.com>
2021-04-03 13:51:28 +0530

    First commit

---
+++ src/DemoJava.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+public class DemoJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to the git project Date and Time::"+ LocalDateTime.now());
+	}
+
+}
